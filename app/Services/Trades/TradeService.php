40c5531cfbc1ba4d68c078b766e660a0cca92004
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-07-23 04:08:59 +0000

    refactor: Enhance TradeService with caching and improve getTrades method for better performance

--- app/Services/Trades/TradeService.php
+++ app/Services/Trades/TradeService.php
@@ -3,19 +3,46 @@
 namespace App\Services\Trades;
 
 use App\Models\Trade;
+use App\Traits\Cacheable;
 
 class TradeService
 {
+
+    use Cacheable;
+
+
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct(
+        protected $cacheTag = 'trades'
+    )
     {
         //
     }
 
-    public static function getTrades()
+    public function getTrades()
     {
-        return Trade::orderBy('trade_name')->get();
+        return $this->rememberCache(
+            'all_trades',
+            function () {
+                return Trade::orderBy('trade_name')->get();
+            }
+        );
+    }
+
+
+    /**
+     * Get all trades as an associative array with trade IDs as keys and names as values.
+     *
+     * @return array
+     */
+    public function getTradesArray()
+    {
+        $trades = $this->getTrades();
+
+        return $trades->mapWithKeys(fn ($trade) => [
+            $trade->id => $trade->trade_name
+        ])->toArray();
     }
 }
