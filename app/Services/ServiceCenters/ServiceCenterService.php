397c8f370b28073e4f056fa8d9e1ed6ef3942962
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-08-22 19:36:19 +0000

    feat: Add getServiceCentersByDistrict method to retrieve service centers by district ID

--- app/Services/ServiceCenters/ServiceCenterService.php
+++ app/Services/ServiceCenters/ServiceCenterService.php
@@ -34,6 +34,23 @@ class ServiceCenterService
     }
 
 
+
+    public function getServiceCentersByDistrict($districtId)
+    {
+        return $this->rememberCache(
+            "service_centers_by_district_{$districtId}",
+            function () use ($districtId) {
+
+                return ServiceCenter::where('district_id', $districtId)
+                    ->with(['district', 'district.region'])
+                    ->orderBy('location')
+                    ->get();
+
+            }
+        );
+    }
+
+
     public function dropCaches()
     {
         $this->forgetCache('all_service_centers');
