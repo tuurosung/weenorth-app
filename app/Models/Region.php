821b8df456c7b0e3b76d18653d5de7cc5cbe02e0
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-24 23:19:24 +0000

    feat: Add member and executive relationships to Region model with attributes for member and district counts

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Executives\RegionalExecutive;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Region extends Model
 {
@@ -24,6 +26,31 @@ class Region extends Model
         'region_name'
     ];
 
+
+    /**
+     * Attributes ------------------------------------------------------------------------------------
+     */
+
+    public function numberOfMembers(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->countMembers()
+        );
+    }
+
+
+    public function numberOfDistricts(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->districts()->count()
+        );
+    }
+
+
+    /**
+     * Relationships --------------------------------------------------------------------------------
+     */
+
     /**
      * Get the districts for the region.
      */
@@ -32,11 +59,29 @@ class Region extends Model
         return $this->hasMany(District::class);
     }
 
+
+    public function members(): HasMany
+    {
+        return $this->hasMany(Member::class, 'region_id', 'id');
+    }
+
+
+    public function executives(): HasMany
+    {
+        return $this->hasMany(RegionalExecutive::class, 'region_id', 'id');
+    }
+
     /**
      * Get the number of districts for the region.
      */
     public function getNumberOfDistrictsAttribute(): int
     {
         return $this->districts()->count();
+    }
+
+
+    private function countMembers(): int
+    {
+        return $this->members()->count();
     }
 }
