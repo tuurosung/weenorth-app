454307af27c91e4ae8a56c947e0922eee73b7f9c
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2025-11-27 23:09:40 +0000

    feat: Update regional executives view with improved header and table styling

--- resources/views/app/the-network/regional-executives.blade.php
+++ resources/views/app/the-network/regional-executives.blade.php
@@ -2,44 +2,55 @@
 
 
 @section('content')
-    <x-headers.top-header pageTitle="Regional Executives">
-        <button class="btn btn-primary">
-            <i class="fi fi-rc-plus me-3"></i>Add Regional Executive</button>
-    </x-headers.top-header>
+    <x-headers.top-header2 title="Regional Executives">
 
-    <div class="card border-0">
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-sm datatables align-middle mb-0">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Member's Name</th>
-                            <th scope="col">Region</th>
-                            <th scope="col">Position</th>
-                            <th class="text-end">Options</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($regionalExecutives as $regionalExecutive)
-                            <tr class="">
-                                <td scope="row">{{ $loop->iteration }}</td>
-                                <td>{{ $regionalExecutive->member->name }}</td>
-                                <td>{{ $regionalExecutive->region->region_name }}</td>
-                                <td>{{ $regionalExecutive->position_name }}</td>
-                                <td class="text-end">
-                                    <a href="" class="text-danger">
-                                        <i class="fi fi-sr-trash"></i>
-                                        Remove
-                                    </a>
-                                </td>
+        <!-- <button class="btn btn-primary btn-sm">
+            <i class="fi fi-rc-plus me-3"></i>
+            Add Regional Executive
+        </button> -->
+
+    </x-headers.top-header2>
+
+
+        <div class="card border-0">
+            <div class="card-body">
+                <div class="table-responsive">
+
+                    <table class="table table-sm datatables align-middle mb-0">
+                        <thead class="table-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Member's Name</th>
+                                <th scope="col">Region</th>
+                                <th scope="col">Position</th>
+                                <th class="text-end">Options</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                        </thead>
+                        <tbody>
 
+                            @foreach ($regionalExecutives as $regionalExecutive)
+
+                                <tr class="">
+                                    <td scope="row">{{ $loop->iteration }}</td>
+                                    <td>{{ $regionalExecutive->member->name }}</td>
+                                    <td>{{ $regionalExecutive->region->region_name }}</td>
+                                    <td>{{ $regionalExecutive->position_name }}</td>
+                                    <td class="text-end">
+                                        <a href="" class="text-danger">
+                                            <i class="fi fi-sr-trash"></i>
+                                            Remove
+                                        </a>
+                                    </td>
+                                </tr>
+
+                            @endforeach
+
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
         </div>
-    </div>
 
 @endsection
