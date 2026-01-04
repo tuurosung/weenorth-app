25894954f86045ab29679e5b70137c85cdf0a000
Tuurosung <16798783+tuurosung@users.noreply.github.com.>
2026-01-04 00:32:19 +0000

    feat: Create ServiceRequest module with initial structure and event binding methods

---
+++ resources/js/modules/service-requests/service-request.js
@@ -0,0 +1,16 @@
+import { HandleRegionChange } from '../plugins/HandleRegionChange.js';
+
+
+export const ServiceRequest = {
+
+    elements: {},
+
+    init() {
+
+    },
+
+    bindEvents() {
+
+    },
+
+}
